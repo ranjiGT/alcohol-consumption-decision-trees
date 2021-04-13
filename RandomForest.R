65bbf47498ce764cb422073d7c98242e33d4333b
Ranji Raj <64376506+ranjiGT@users.noreply.github.com>
2021-04-13 08:57:57 +0200

    Update RandomForest.R

--- RandomForest.R
+++ RandomForest.R
@@ -11,8 +11,5 @@ sens <- cm[1,1]/sum(cm[1,])
 sens
 spec <- cm[2,2]/sum(cm[2,])
 spec
-#...from ?importance
-# Here are the definitions of the variable importance measures. 
-#The first measure is computed from 
-#The second measure is the total decrease in node impurities from splitting on the variable, averaged
+
 varImpPlot(rf, type = 2)
